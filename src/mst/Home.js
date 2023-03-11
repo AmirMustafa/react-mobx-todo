62f06fa4e4fd1b29c5969c72081f87878e22d764
AMIR MUSTAFA <amirengg15@gmail.com>
2023-03-11 23:16:41 +0530

    feat: fetching store data, computable data (total marks - getter)

--- src/mst/Home.js
+++ src/mst/Home.js
@@ -5,6 +5,10 @@ function Home() {
     return (
         <div>
             <h1>{store.userName} - {store.userId}</h1>
+            <h2>Math marks: {store.math}</h2>
+            <h2>Hindi marks: {store.hindi}</h2>
+            <h2>Total marks: {store.totalMarks}</h2>
+            {/* <button onClick={}>Update Hindi</button> */}
         </div>
     );
 }
